64c845dee2d90db47bb2648a067dd51428fa2563
Benjamin Bour <benjamin.bour@insee.fr>
2022-05-25 15:00:17 +0200

    Delete README

--- src/main/java/fr/insee/pocasync/producer/service/UserServiceImpl.java
+++ src/main/java/fr/insee/pocasync/producer/service/UserServiceImpl.java
@@ -25,7 +25,7 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public String createUser(String username) {
-    
+
     String correlationId=UUID.randomUUID().toString();
     userProducer.publish(username, correlationId);
     responseReceiverFromConsumer.receiveResponse(MESSAGE_QUEUE_RESPONSE, correlationId);
