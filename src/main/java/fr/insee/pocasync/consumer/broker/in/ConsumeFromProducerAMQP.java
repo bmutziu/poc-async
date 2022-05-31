795f08638692296194d08efef9b2e36d7c431bf3
Benjamin Bour <benjamin.bour@insee.fr>
2022-05-31 17:12:18 +0200

    Update AMQP

--- src/main/java/fr/insee/pocasync/consumer/broker/in/ConsumeFromProducerAMQP.java
+++ src/main/java/fr/insee/pocasync/consumer/broker/in/ConsumeFromProducerAMQP.java
@@ -1,16 +1,29 @@
 package fr.insee.pocasync.consumer.broker.in;
+import fr.insee.pocasync.ConfigurationAMQP;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
 @ConditionalOnProperty(prefix= "notification", name = "service", havingValue = "amqp")
+@RequiredArgsConstructor
 public class ConsumeFromProducerAMQP {
 
-    public void receiveMessage(String message) {
+    @SneakyThrows
+    @RabbitListener(queues = ConfigurationAMQP.MESSAGE_QUEUE_REQUEST)
+    public String receiveMessage(String name) {
         log.info("##################################");
-        log.info("RABBITMQ - CONSUMER : received message : " + message);
+        log.info("RABBITMQ - CONSUMER : received message");
         log.info("##################################");
+
+        TimeUnit.SECONDS.sleep(10);
+
+        return "Hello world, " + name;
     }
 }
--- src/main/java/fr/insee/pocasync/producer/broker/out/RequestToConsumerAMQP.java
+++ src/main/java/fr/insee/pocasync/producer/broker/out/RequestToConsumerAMQP.java
@@ -1,28 +1,65 @@
 package fr.insee.pocasync.producer.broker.out;
 
 import fr.insee.pocasync.ConfigurationAMQP;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.AsyncRabbitTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import javax.transaction.Transactional;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @ConditionalOnProperty(prefix = "notification", name = "service", havingValue = "amqp")
+@RequiredArgsConstructor
 @Component
 public class RequestToConsumerAMQP {
 
     private final RabbitTemplate rabbitTemplate;
 
-    public RequestToConsumerAMQP(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
+    private final AsyncRabbitTemplate asyncRabbitTemplate;
 
-    public void publish(String username) {
+    @Value("${notification.service.mode}")
+    private String mode;
+
+    @Transactional
+    public String publish(String username) {
+
         log.info("##################################");
         log.info("RABBITMQ - PRODUCER : send message");
         log.info("##################################");
 
-        rabbitTemplate.convertAndSend(ConfigurationAMQP.MESSAGE_QUEUE_REQUEST, "foo.bar.baz", "Hello from RabbitMQ!");
-    }
+        if ("sync".equals(mode)) {
+            rabbitTemplate.setReplyTimeout(60000);
 
+            String response = (String) rabbitTemplate.convertSendAndReceive(
+                    ConfigurationAMQP.EXCHANGE_NAME,
+                    ConfigurationAMQP.ROUTING_KEY,
+                    username);
+
+            return response;
+
+        } else {
+            ListenableFuture<String> listenableFuture =
+                    asyncRabbitTemplate.convertSendAndReceive(
+                            ConfigurationAMQP.EXCHANGE_NAME,
+                            ConfigurationAMQP.ROUTING_KEY,
+                            username);
+            // non blocking part
+            log.info("Non blocking block");
+
+            try {
+                String response = listenableFuture.get();
+                log.info("Message received: {}", response);
+                return response;
+            } catch (InterruptedException | ExecutionException e) {
+                log.error("Cannot get response.", e);
+            }
+        };
+        return null;
+    }
 }
