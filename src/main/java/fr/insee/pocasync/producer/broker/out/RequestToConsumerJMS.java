9965ca9a8a461d1988d537953da7c31a304d80fd
Benjamin Bour <benjamin.bour@insee.fr>
2022-05-31 11:26:55 +0200

    First commit

---
+++ src/main/java/fr/insee/pocasync/producer/broker/out/RequestToConsumerJMS.java
@@ -0,0 +1,32 @@
+package fr.insee.pocasync.producer.broker.out;
+
+import fr.insee.pocasync.ConfigurationJMS;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+
+@Slf4j
+@Component
+@ConditionalOnProperty(prefix = "notification", name = "service", havingValue = "jms")
+public class RequestToConsumerJMS {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Transactional
+    public void publish(String username, String jmsCorrelationId) {
+
+        log.info("##################################");
+        log.info("ACTIVEMQ - PRODUCER : send message with correlation_id: <" + jmsCorrelationId + ">");
+        log.info("##################################");
+
+        jmsTemplate.convertAndSend(ConfigurationJMS.MESSAGE_QUEUE_REQUEST, "JMS received User : " + username, m -> {
+            m.setJMSCorrelationID(jmsCorrelationId);
+            return m;
+        });
+    }
+}
